79d7f4391ea604fda4b7b83330c4c468d8ba9ef1
Tingting Zhao <RexRowan@users.noreply.github.com>
2016-08-20 17:36:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 <script>
 
-function greeting() {
-  var name=prompt("What is your name?");
-  alert("Welcome to my website " + name);
-  }
-  
-  greeting();
-  
+
 $("body").addClass("animated hinge");
 </script>
